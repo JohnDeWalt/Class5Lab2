027eb8b0cc0e8973db24ac79aa823b9541dd69c3
JohnDeWalt <Jackie@10.30.12.25>
2017-09-14 13:26:29 -0500

    Absract version

--- src/newabstract/Amphibian.java
+++ src/newabstract/Amphibian.java
@@ -9,6 +9,51 @@ package newabstract;
  *
  * @author Jackie
  */
-public class Amphibian {
+public class Amphibian extends Animal {
+    private boolean coldBlooded; // mush be cold blooded to be an amphibian
+    private boolean permeableSkin; //must have permeable skin to be a amphibian
+    private boolean ecosystem; //must live on land and water
+    
+//    @Override
+//    public boolean getMate(){
+//        if(coldBlooded == false && permeableSkin == false){
+//            throw new IllegalArgumentException("This animal is not an amphibain and cannot reproduce with one");
+//        }else{
+//            return true;
+//        }
+//    }
+
+    public boolean getColdBlooded() {
+        return coldBlooded;
+    }
+
+    public void setColdBlooded(boolean coldBlooded) {
+        if(coldBlooded == false){
+            throw new IllegalArgumentException("This animal is not an amphibian");
+        }
+        this.coldBlooded = coldBlooded;
+    }
+
+    public boolean getPermeableSkin() {
+        return permeableSkin;
+    }
+
+    public void setPermeableSkin(boolean permeableSkin) {
+        if(permeableSkin == false){
+            throw new IllegalArgumentException("This animal is not an amphibian");
+        }
+        this.permeableSkin = permeableSkin;
+    }
+
+    public boolean getEcosystem() {
+        return ecosystem;
+    }
+
+    public void setEcosystem(boolean ecosystem) {
+        if(ecosystem == false){
+            throw new IllegalArgumentException("This animal is not an amphibian");
+        }
+        this.ecosystem = ecosystem;
+    }
     
 }
--- src/newabstract/Animal.java
+++ src/newabstract/Animal.java
@@ -9,6 +9,48 @@ package newabstract;
  *
  * @author Jackie
  */
-public class Animal {
+public abstract class Animal {
+    
+    private String eat;
+    private String drink;
+    private String reproduce;
+    
+    public boolean getMate(){
+        return false; 
+    }
+
+    public String getEat() {
+        return eat;
+    }
+
+    public void setEat(String eat) {
+        if(eat == null){
+            throw new IllegalArgumentException("Animals must eat to survive");
+        }
+        this.eat = eat;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public void setDrink(String drink) {
+        if(drink == null){
+            throw new IllegalArgumentException("Animals must drink to survive");
+        }
+        this.drink = drink;
+    }
+
+    public String getReproduce() {
+        return reproduce;
+    }
+
+    public void setReproduce(String reproduce) {
+        if(reproduce == null){
+            throw new IllegalArgumentException("Animals must reproduce to keep the species alive"); 
+        }
+        this.reproduce = reproduce;
+    }
+
     
 }
